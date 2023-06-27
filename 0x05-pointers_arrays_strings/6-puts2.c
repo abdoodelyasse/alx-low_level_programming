947c777243d870fb1203bddec4977c291a103dd6
Abdelmounim Delyasse <Abdelmounim.DELYASSE@emines.um6p.ma>
2023-06-27 16:20:35 -0700

    o

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
 
 /**
  * puts2 - Prints every other character of a string, starting with the first character,
@@ -7,12 +9,17 @@
  */
 void puts2(char *str)
 {
+    setlocale(LC_ALL, ""); // Set the locale to handle UTF-8 characters
+
+    wchar_t wide_str[100]; // Assuming a maximum string length of 100
+    mbstowcs(wide_str, str, 100); // Convert the string to wide characters
+
     int i;
 
-    for (i = 0; str[i] != '\0'; i += 2)
+    for (i = 0; wide_str[i] != L'\0'; i += 2)
     {
-        putchar(str[i]);
+        putwchar(wide_str[i]);
     }
 
-    putchar('\n');
+    putwchar(L'\n');
 }
