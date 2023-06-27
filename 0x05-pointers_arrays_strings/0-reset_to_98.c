251adc23692f02051988dd808d66793619a33614
Abdelmounim Delyasse <Abdelmounim.DELYASSE@emines.um6p.ma>
2023-06-27 15:58:57 -0700

    o

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,10 @@
+/**
+ * reset_to_98 - Updates the value pointed to by a pointer to 98
+ * @n: Pointer to an integer
+ *
+ * Return: void
+ */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
