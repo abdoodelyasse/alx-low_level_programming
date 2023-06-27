de47cd0ef77a1851e34eb2ff0d279bd005864dcc
Abdelmounim Delyasse <Abdelmounim.DELYASSE@emines.um6p.ma>
2023-06-27 16:26:23 -0700

    l

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
-#include <wchar.h>
-#include <locale.h>
 
 /**
- * puts2 - Prints every other character of a string, starting with the first character,
- *         followed by a new line.
+ * puts2 - Prints every other character of a string, starting with the first character, followed by a new line.
  * @str: The input string.
  */
 void puts2(char *str)
 {
-    setlocale(LC_ALL, ""); // Set the locale to handle UTF-8 characters
+	if (str == NULL)
+		return;
 
-    wchar_t wide_str[100]; // Assuming a maximum string length of 100
-    mbstowcs(wide_str, str, 100); // Convert the string to wide characters
+	int i = 0;
 
-    int i;
+	while (str[i] != '\0')
+	{
+		printf("%c", str[i]);
+		i += 2;
+	}
 
-    for (i = 0; wide_str[i] != L'\0'; i += 2)
-    {
-        putwchar(wide_str[i]);
-    }
-
-    putwchar(L'\n');
+	printf("\n");
 }
