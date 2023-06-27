423cb3f3d4296100b01b9986cf080d8b8f4dcd7d
Abdelmounim Delyasse <Abdelmounim.DELYASSE@emines.um6p.ma>
2023-06-27 16:38:35 -0700

    l

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * puts2 - Prints every other character of a string, starting with the first character, followed by a new line.
+ * puts2 - Prints every other character of a string, starting with the first
+ *         character, followed by a new line.
  * @str: The input string.
  */
 void puts2(char *str)
