c7a34dd737c355c96495ed0c3c827cc1a929547a
Abdelmounim Delyasse <Abdelmounim.DELYASSE@emines.um6p.ma>
2023-06-27 18:31:18 -0700

    l

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,44 +9,41 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int i = 0;
+    int sign = 1;
+    int result = 0;
+    int i = 0;
 
-	/* Handle leading whitespace */
-	while (s[i] == ' ')
-		i++;
+    /* Handle leading whitespace */
+    while (s[i] == ' ')
+        i++;
 
-	/* Handle sign */
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (s[i] == '+')
-	{
-		i++;
-	}
+    /* Handle sign */
+    while (s[i] == '-' || s[i] == '+')
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        i++;
+    }
 
-	/* Convert string to integer */
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		/* Check for integer overflow */
-		if (result > (INT_MAX - (s[i] - '0')) / 10)
-		{
-			if (sign == 1)
-			{
-				return INT_MAX;
-			}
-			else
-			{
-				return INT_MIN;
-			}
-		}
+    /* Convert string to integer */
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        /* Check for integer overflow */
+        if (result > (INT_MAX - (s[i] - '0')) / 10)
+        {
+            if (sign == 1)
+            {
+                return INT_MAX;
+            }
+            else
+            {
+                return INT_MIN;
+            }
+        }
 
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
 
-	return result * sign;
+    return result * sign;
 }
