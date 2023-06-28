6fd6eae8bbc51b3c6e11ca3cfdcd340e0ad6c96b
Abdelmounim Delyasse <Abdelmounim.DELYASSE@emines.um6p.ma>
2023-06-27 18:22:37 -0700

    l

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The input string.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	/* Handle leading whitespace */
+	while (s[i] == ' ')
+		i++;
+
+	/* Handle sign */
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
+
+	/* Convert string to integer */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		/* Check for integer overflow */
+		if (result > (INT_MAX - (s[i] - '0')) / 10)
+		{
+			if (sign == 1)
+			{
+				return INT_MAX;
+			}
+			else
+			{
+				return INT_MIN;
+			}
+		}
+
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return result * sign;
+}
